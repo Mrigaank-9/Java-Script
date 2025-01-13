6348b542b80035de9a077c77b822ca143899b527
Mrigaank-9 <130607343+Mrigaank-9@users.noreply.github.com>
2025-01-14 00:21:13 +0530

    Watch again

---
+++ session22.js
@@ -0,0 +1,14 @@
+// js Exceution context
+
+/*
+code -> Glovbal Excecution context (stores in this)
+
+3 contexts 
+1. Global EC 
+2. Function EC
+3. Eval EC ( in moogose)
+
+{code} -> memory creation phase -> exceution phase 
+
+video to watch still - https://www.youtube.com/watch?v=ByhtOgF6uYM&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=25
+*/
